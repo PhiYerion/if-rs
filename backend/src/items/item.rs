7ec7a839c99d049b02d370be4f0987253d1cf7fc
Reid Bandy <reidbandy@gmail.com>
2024-03-23 19:59:44 -0400

    New item system

--- backend/src/items/item.rs
+++ backend/src/items/item.rs
@@ -1,9 +1,44 @@
 use std::fmt::Debug;
 
-use super::ItemType;
+#[derive(Debug, Clone, Copy, PartialEq, Eq, PartialOrd, Ord)]
+pub enum ItemWeight {
+    Continuous(f32),
+    Discrete(usize),
+}
 
-pub trait ItemTrait: 'static + Sync + Send + Debug {
-    fn amount(&self) -> String;
-    fn get_type(&self) -> &'static dyn ItemType;
+/// # Examples:
+/// ```
+/// struct IronOre {
+///     id: usize,
+///     amount: f32,
+///     purity: f32,
+/// }
+///
+/// // These functions will be in a context where the type does not matter. For instance, when
+/// // listing the amounts of all of the items in a list.
+/// impl Item for IronOre {
+///     fn type_name(&self) -> &'static str { "Iron Ore" }
+///     fn type_description(&self) -> &'static str { "A rock containing iron." }
+///     fn amount(&self) -> ItemWeight { ItemWeight::Continuous(self.amount) }
+///     fn id(&self) -> usize { self.id }
+/// }
+///
+/// // This will be used when the type does matter and specific operations need to be preformed on
+/// // the item, specific to the item. Weapons cannot be purified, and contraining it to this type
+/// // will ensure that they will only be called when valid.
+/// impl IronOre {
+///     pub fn purify(&mut self, amount: f32) {
+///         let purity_change = (1 - self.purity) * amount;
+///         self.purity += purity_change;
+///         self.amount *= (1 - purity_change);
+///     }
+/// }
+/// ```
+
+pub trait Item: 'static + Sync + Send + Debug {
+    fn type_name(&self) -> &'static str;
+    fn type_description(&self) -> &'static str;
+    fn amount(&self) -> ItemWeight;
     fn id(&self) -> usize;
 }
+
